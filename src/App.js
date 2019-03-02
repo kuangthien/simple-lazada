0f2c0243aeae38465704a79fa6e4fec2cd087659
Tran Kuang Thien <kuangthien@gmail.com>
2019-03-02 17:00:11 +0700

    setup booostrap, setup jsonserver

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./assets/styles/App.scss";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    axios
+      .get(process.env.REACT_APP_PATH_API + "test-api")
+
+      .then(result => {
+        const { success } = result.data;
+        return this.setState({
+          success
+        });
+      })
+      .catch(error =>
+        this.setState({
+          error
+        })
+      );
+  }
   render() {
+    const printState = JSON.stringify(this.state);
     return (
       <div className="App">
+        <div className="container">
+          Test API:
+          {printState}
+          <div className="row">
+            <div className="col">Test grid</div>
+            <div className="col">Test grid</div>
+          </div>
+        </div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
